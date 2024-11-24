9ccad59718479797561805fba1e8957998b68389
Art Vardanyan <akvardanyan99@gmail.com>
2024-11-24 18:39:57 -0500

    wip

--- src/components/hero-section/Hero.jsx
+++ src/components/hero-section/Hero.jsx
@@ -3,7 +3,16 @@ import './hero.css'
 const Hero = () => {
     return (
         <div className="hero-container">
-            
+            <div className="company-name">
+                <div className="one">Carolinas</div>
+                <div className="two">
+                <i class="fa-solid fa-truck-fast"></i><div>Courier</div>
+                </div>
+                <div className="three">Services</div>
+            </div>
+            <div className="hero-slogan">
+                Driven to deliver.
+            </div>
         </div>
     );
 }
