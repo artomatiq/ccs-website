75933b69afa173d5682dba5021ce77c241a37b20
Art Vardanyan <akvardanyan99@gmail.com>
2024-12-20 18:22:24 -0500

    date validation pop-ups

--- src/utils/DateValidation.js
+++ src/utils/DateValidation.js
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2"
+
 const handleDateBlur = (e, formData, setFormData) => {
     const name = e.target.name
     const value = e.target.value
@@ -5,31 +7,75 @@ const handleDateBlur = (e, formData, setFormData) => {
         const todayDate = new Date()
         const pickupDate = new Date(value)
         if (pickupDate <= todayDate) {
-            alert('Please select a pick-up date that is later than today.')
-            setFormData(prevState => ({ ...prevState, [name]: '' }));
-            return
+            Swal.fire({
+                title: 'Invalid Date',
+                text: 'Please select a pick-up date that is later than today.',
+                icon: 'warning',
+                customClass: {
+                    container: 'swal-container',
+                    popup: 'swal-popup',
+                    title: 'swal-title',
+                    content: 'swal-content',
+                    confirmButton: 'swal-confirm-button'
+                }
+            });
+            setFormData(prevState => ({ ...prevState, [name]: '' }))
+            return;
         }
         const sixMonthsAfterToday = new Date();
         sixMonthsAfterToday.setMonth(sixMonthsAfterToday.getMonth() + 6);
         if (pickupDate > sixMonthsAfterToday) {
-            alert('Please select a pick-up date that is no more than 6 months away.')
-            setFormData(prevState => ({ ...prevState, [name]: '' }));
-            return
+            Swal.fire({
+                title: 'Invalid Date',
+                text: 'Please select a pick-up date that is no more than 6 months away.',
+                icon: 'warning',
+                customClass: {
+                    container: 'swal-container',
+                    popup: 'swal-popup',
+                    title: 'swal-title',
+                    content: 'swal-content',
+                    confirmButton: 'swal-confirm-button'
+                }
+            });
+            setFormData(prevState => ({ ...prevState, [name]: '' }))
+            return;
         }
     } else if (name === 'dropoffDate') {
         const pickupDate = new Date(formData.pickupDate);
         const dropoffDate = new Date(value);
         if (dropoffDate < pickupDate) {
-            alert('Please select a drop-off date that is later than the pick-up date.');
-            setFormData(prevState => ({ ...prevState, [name]: '' }));
-            return
+            Swal.fire({
+                title: 'Invalid Date',
+                text: 'Please select a drop-off date that is later than the pick-up date.',
+                icon: 'warning',
+                customClass: {
+                    container: 'swal-container',
+                    popup: 'swal-popup',
+                    title: 'swal-title',
+                    content: 'swal-content',
+                    confirmButton: 'swal-confirm-button'
+                }
+            });
+            setFormData(prevState => ({ ...prevState, [name]: '' }))
+            return;
         }
         const oneMonthAfterPickup = new Date(pickupDate);
         oneMonthAfterPickup.setMonth(pickupDate.getMonth() + 1);
         if (dropoffDate > oneMonthAfterPickup) {
-            alert('Please select a drop-off date that is no more than one month after the pick-up date.');
-            setFormData(prevState => ({ ...prevState, [name]: '' }));
-            return
+            Swal.fire({
+                title: 'Invalid Date',
+                text: 'Please select a drop-off date that is no more than one month after the pick-up date.',
+                icon: 'warning',
+                customClass: {
+                    container: 'swal-container',
+                    popup: 'swal-popup',
+                    title: 'swal-title',
+                    content: 'swal-content',
+                    confirmButton: 'swal-confirm-button'
+                }
+            });
+            setFormData(prevState => ({ ...prevState, [name]: '' }))
+            return;
         }
     }
 }
