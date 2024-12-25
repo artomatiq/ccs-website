b33d8dde6994f0019b90e7adbef033c44522e027
Art Vardanyan <akvardanyan99@gmail.com>
2024-12-24 22:26:42 -0500

    wip

--- src/utils/handleAppointmentClick.js
+++ src/utils/handleAppointmentClick.js
@@ -1,7 +1,6 @@
 import Swal from "sweetalert2";
 
-// const isTouchDevice = 'ontouchstart' in window && (window.innerWidth <= 1024);
-const isTouchDevice = true
+const isTouchDevice = 'ontouchstart' in window && (window.innerWidth <= 1024);
 
 const fireSwal = (inputName) => {
     Swal.fire({
@@ -19,31 +18,22 @@ const fireSwal = (inputName) => {
 }
 
 const handleAppointmentClick = (e) => {
-    console.log('handling app click')
 
     const pickupDateInputId = 'origin-date'
     const pickupTimeInputId = 'origin-time'
     const dropoffDateInputId = 'dest-date'
-
     const pickupDateInput = document.querySelector(`#${pickupDateInputId}`)
     const pickupTimeInput = document.querySelector(`#${pickupTimeInputId}`)
     const dropoffDateInput = document.querySelector(`#${dropoffDateInputId}`)
-
     const inputArray = [dropoffDateInput, pickupTimeInput, pickupDateInput]
 
     const target = e.target.tagName === 'INPUT' ? e.target : e.target.querySelector('input');
     if (!isTouchDevice || !target || !target.disabled) return
 
-    if (!pickupDateInput || !pickupTimeInput || !dropoffDateInput) {
-        console.error('One or more input elements are missing!', pickupDateInput, pickupTimeInput, dropoffDateInput);
-        return;
-    }
-
     for (let input of inputArray) {
-        console.log(input, 'disabled? ', input.disabled);
         if (!input.disabled) {
             fireSwal(input.dataset.name);
-            break; // Exit the loop after showing the alert
+            break;
         }
     }
 
