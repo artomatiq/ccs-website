4c26f38e7e74eed119ffe73ba793c946ff6a4d70
Art Vardanyan <akvardanyan99@gmail.com>
2024-12-30 07:24:21 -0500

    google maps api key in environment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
+import { LoadScript } from '@react-google-maps/api';
+
+const libraries = ['places']
+const googleMapsPublicKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <LoadScript googleMapsApiKey={googleMapsPublicKey} libraries={libraries}>
+        <App />
+      </LoadScript>
     </BrowserRouter>
   </React.StrictMode>
 );
