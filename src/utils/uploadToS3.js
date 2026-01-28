86c6eacd6a36c8e3b07a2c32d8b4a965f31cf0b3
Art Vardanyan <akvardanyan99@gmail.com>
2026-01-28 17:34:07 -0500

    uploadToS3.js

--- src/utils/uploadToS3.js
+++ src/utils/uploadToS3.js
@@ -1,3 +1,27 @@
-export default function uploadToS3 () {
-    
+export default async function uploadToS3(ticket, token) {
+    try {
+        //get presigned url
+        const presignRes = await fetch(process.env.REACT_APP_API_BASE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({fileType: ticket.type})
+        })
+        if (!presignRes.ok) throw new Error("Failed to get presigned URL")
+        const { uploadURL, key, ticketId } = await presignRes.json()
+
+        //upload to s3
+        const uploadRes = await fetch (uploadURL, {
+            method: "PUT",
+            headers: {"Content-Type": ticket.type},
+            body: ticket
+        })
+        if (!uploadRes.ok) throw new Error("S3 upload failed")
+        return { key, ticketId }
+    } catch (err) {
+        console.error("uploadToS3 error:", err)
+        throw err
+    }
 }
