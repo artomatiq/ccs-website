e3ea9176b57ae8beddf8796f23f71cb1ba9772ad
Art <126132705+artomatic@users.noreply.github.com>
2026-03-23 21:47:31 -0400

    wip

--- src/components/ticket/ReviewPage.jsx
+++ src/components/ticket/ReviewPage.jsx
@@ -4,22 +4,27 @@ import imgUrl from "../../assets/60b65df1-392f-401f-91b9-dea5173562df.png"
 import TicketOverlay from "./TicketOverlay"
 
 export default function ReviewPage({ dbTicket, setDbTicket }) {
-    const [imgLoaded, setImgLoaded] = useState(false)
+    const [imgLoaded, setImgLoaded] = useState(true)
     const [imgError, setImgError] = useState(false)
 
     if (imgError) {
         return <div>Failed to load image.</div>
     }
     return (
-        <div>
+        <div className="review-wrapper">
             {!imgLoaded && <div>Fetching ticket...</div>}
-            <img
-                // src={dbTicket.downloadUrl}
-                src={imgUrl}
-                alt="ValidTicketImg"
-                onLoad={() => setImgLoaded(true)}
-                onError={() => setImgError(true)}
-            />
+            {imgLoaded && (
+                <div className="ticket-container">
+                    <img
+                        src={imgUrl}
+                        alt="ValidTicketImg"
+                        className="ticket-image"
+                        onLoad={() => setImgLoaded(true)}
+                        onError={() => setImgError(true)}
+                    />
+                    <TicketOverlay dbTicket={dbTicket} />
+                </div>
+            )}
         </div>
     )
 }
