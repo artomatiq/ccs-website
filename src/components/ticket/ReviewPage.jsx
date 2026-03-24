439d49295abe05c01b00978bd55bcf80527fe499
Art <126132705+artomatic@users.noreply.github.com>
2026-03-23 22:25:50 -0400

    wip

--- src/components/ticket/ReviewPage.jsx
+++ src/components/ticket/ReviewPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./reviewPage.css"
 import imgUrl from "../../assets/60b65df1-392f-401f-91b9-dea5173562df.png"
 import TicketOverlay from "./TicketOverlay"
@@ -6,6 +6,16 @@ import TicketOverlay from "./TicketOverlay"
 export default function ReviewPage({ dbTicket, setDbTicket }) {
     const [imgLoaded, setImgLoaded] = useState(true)
     const [imgError, setImgError] = useState(false)
+
+    const handleConfirm = async (e) => {
+        e.preventDefault()
+    }
+
+    useEffect( () => {
+        const element = document.querySelector(".review-wrapper")
+        const scroll = () => window.scrollTo(0, element.offsetTop)
+        if (element) scroll()
+    }, [])
 
     if (imgError) {
         return <div>Failed to load image.</div>
@@ -25,6 +35,14 @@ export default function ReviewPage({ dbTicket, setDbTicket }) {
                     <TicketOverlay dbTicket={dbTicket} />
                 </div>
             )}
+            <button
+                type="button"
+                onClick={handleConfirm}
+                className="button"
+                // disabled={!attachment}
+            >
+                Submit
+            </button>
         </div>
     )
 }
