62a247c04d37709858816ed44ccfda2fb894642b
Art <126132705+artomatic@users.noreply.github.com>
2026-04-14 11:48:31 -0400

    hero styling

--- src/components/hero-section/Hero.jsx
+++ src/components/hero-section/Hero.jsx
@@ -1,21 +1,21 @@
-import './hero.css'
+import "./hero.css"
 
 const Hero = () => {
     return (
-        <div className="hero-container hide" id='home-id'>
-            <div className="company-name">
-                <div className="one">Carolinas</div>
-                <div className="two">
-                    <i className="fa-solid fa-truck-fast"></i>
-                    <div>Courier</div>
+        <div className="hero-precontainer">
+            <div className="hero-container hide" id="home-id">
+                <div className="company-name">
+                    <div className="one">Carolinas</div>
+                    <div className="two">
+                        <i className="fa-solid fa-truck-fast"></i>
+                        <div>Courier</div>
+                    </div>
+                    <div className="three">Services</div>
                 </div>
-                <div className="three">Services</div>
-            </div>
-            <div className="hero-slogan">
-                Driven to deliver.
+                <div className="hero-slogan">Driven to deliver.</div>
             </div>
         </div>
-    );
+    )
 }
 
-export default Hero;
+export default Hero
