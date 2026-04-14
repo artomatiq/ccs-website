758aac9e292fdb4921c41fa0443f5f0fb7f85c31
Art <126132705+artomatic@users.noreply.github.com>
2026-04-14 00:18:41 -0400

    wip

--- src/components/ticket/Ticket.jsx
+++ src/components/ticket/Ticket.jsx
@@ -8,20 +8,6 @@ import { useLocation } from "react-router-dom"
 import { Routes, Route } from "react-router-dom"
 
 const Ticket = () => {
-    // useEffect(() => {
-    //     const button = document.querySelector(".ticket-button.nav-button")
-    //     const element = document.querySelector(".ticket-container")
-    //     const scroll = () => window.scrollTo(0, element.offsetTop)
-    //     if (element) {
-    //         scroll()
-    //         if (button) button.addEventListener("click", scroll)
-    //         return () => {
-    //             if (button) button.removeEventListener("click", scroll)
-    //         }
-    //     }
-    // }, [])
-
-    // const isMobile = true ///Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
     const { isAuthenticated } = useAuth()
     const navigate = useNavigate()
     const location = useLocation()
@@ -61,19 +47,5 @@ const Ticket = () => {
             </Routes>
         </div>
     )
-
-    // return isMobile ? (
-    //     <div className="quote-container ticket-container section" id="about-id">
-    //         {/* <div className="title section segment">
-    //             {isAdmin && <span className="hide">Welcome</span>}
-    //             {!isAdmin && <span className="hide">Submit Hauling Ticket</span>}
-    //         </div> */}
-    //         {isAuthenticated ? <TicketWorkflow /> : <PasscodePage />}
-    //     </div>
-    // ) : (
-    //     <div>
-    //         <h3>Please use a smartphone.</h3>
-    //     </div>
-    // )
 }
 export default Ticket
