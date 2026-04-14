26c92d77bf32caea6a6911a180b05a8b536026dd
Art <126132705+artomatic@users.noreply.github.com>
2026-04-14 12:16:23 -0400

    wip

--- src/components/pre-loader/PreLoader.jsx
+++ src/components/pre-loader/PreLoader.jsx
@@ -1,23 +1,26 @@
-import './pre-loader.css'
-import { useEffect } from 'react';
+import "./pre-loader.css"
+import { useEffect } from "react"
+
+import symbol from "../../assets/eternity.png"
 
 const PreLoader = () => {
-
     useEffect(() => {
         const timeout = setTimeout(() => {
-            document.querySelector('.preloader-container div')?.classList.add('show')
-        }, 700);
+            document
+                .querySelector(".preloader-container div")
+                ?.classList.add("show")
+            document.querySelector("#symbol-png")?.classList.add("show")
+        }, 700)
 
         return () => clearTimeout(timeout)
-    }, []);
-
-    
+    }, [])
 
     return (
-        <div className='preloader-container'>
+        <div className="preloader-container">
             <div>Carolinas Courier Services</div>
+            <img src={symbol} id="symbol-png" />
         </div>
-    );
+    )
 }
 
-export default PreLoader;
+export default PreLoader
